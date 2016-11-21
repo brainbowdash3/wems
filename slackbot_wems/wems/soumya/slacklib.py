9956faee2d4377173d7fad90f319d174c87e6cd9
brainbowdash3 <skappie09@hotmail.com>
2016-11-21 11:28:53 -0500

    Soumya adding ten breeds of dogs command
    Command 2

--- slackbot_wems/wems/soumya/slacklib.py
+++ slackbot_wems/wems/soumya/slacklib.py
@@ -1,5 +1,6 @@
 # Put your commands here
 COMMAND1 = "name a bird"
+COMMAND2 = "name ten breeds of dogs"
 
 # Your handling code goes in this function
 def handle_command(command):
@@ -10,6 +11,7 @@ def handle_command(command):
     response = ""
     if command.find(COMMAND1) >= 0:
         response = "Owl"
-        
+    elif command.find(COMMAND2) >= 0:
+        response = "Poodle,Pug,Bulldog,Bloodhound,Foxhound I don't know any more."   
     return response
 
